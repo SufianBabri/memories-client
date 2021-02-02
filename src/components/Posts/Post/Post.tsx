4629e1ecb14977aeb5b9122fd4ece096c9c0c703
Sufian <sufian.babri@gmail.com>
2021-02-02 14:38:48 +0500

    Moved like post implementation into a custom hook.

--- src/components/Posts/Post/Post.tsx
+++ src/components/Posts/Post/Post.tsx
@@ -11,12 +11,9 @@ import ThumbsUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import moment from 'moment';
 import useStyles from './styles';
-import * as api from '../../../data/api';
 import PostModel from '../../../models/postModel';
-import { queryCache, useMutation } from 'react-query';
-import { ALL_POSTS } from '../../../constants/apiPredicates';
-import SnackbarContext from '../../../context/SnackbarContext';
 import useDeletePost from '../../../data/hooks/useDeletePost';
+import useLikePost from '../../../data/hooks/useLikePost';
 import { useError } from '../../../hooks/useSnackbar';
 
 interface Props {
@@ -25,65 +22,13 @@ interface Props {
 
 const Post = ({ post }: Props) => {
 	const classes = useStyles();
-	const snackbarContext = useContext(SnackbarContext);
 
 	const { deletePost, errorOnDelete } = useDeletePost();
+	const { likePost, errorOnLike } = useLikePost();
+
 	useError(errorOnDelete);
+	useError(errorOnLike);
 
-	const [likePost] = useMutation<
-		PostModel,
-		Error,
-		PostModel,
-		PostModel | undefined
-	>((post) => api.likePost(post._id), {
-		onMutate: (updatedPost) => {
-			queryCache.cancelQueries(ALL_POSTS);
-
-			const posts = queryCache.getQueryData<PostModel[]>(ALL_POSTS) ?? [];
-			const found = posts.find((p) => p._id === updatedPost._id);
-			if (!found) {
-				console.error('Post not found!');
-				return undefined;
-			}
-			const snapshot = { ...found };
-
-			queryCache.setQueryData<PostModel[]>(ALL_POSTS, function (current) {
-				updatedPost.likeCount++;
-
-				if (current === undefined) return [updatedPost];
-
-				return current.map((p) => {
-					if (p._id === updatedPost._id) return updatedPost;
-					else return p;
-				});
-			});
-
-			return snapshot;
-		},
-		onError: (e, post, snapshot) => {
-			if (snapshot === undefined) return;
-
-			snackbarContext.setContent({
-				text: 'Could not save your like!',
-				type: 'error',
-			});
-
-			const posts = queryCache.getQueryData<PostModel[]>(ALL_POSTS) ?? [];
-			const updatedPosts = posts.map((p) => {
-				if (p._id === snapshot._id) return snapshot;
-				return p;
-			});
-			queryCache.setQueryData<PostModel[]>(ALL_POSTS, updatedPosts);
-		},
-		onSuccess: (post) => {
-			const posts = queryCache.getQueryData<PostModel[]>(ALL_POSTS) ?? [];
-
-			posts.map((p) => {
-				if (p._id === post._id) return post;
-				return p;
-			});
-		},
-	});
 	return (
 		<Card className={classes.card}>
 			<CardMedia
---
+++ src/data/hooks/useLikePost.ts
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import { queryCache, useMutation } from 'react-query';
+import { ALL_POSTS } from '../../constants/apiPredicates';
+import PostModel from '../../models/postModel';
+import * as api from '../api';
+import ApiError from './ApiError';
+
+export default function useDeletePost() {
+	const [error, setError] = useState<ApiError | undefined>();
+	const [likePost] = useMutation<
+		PostModel,
+		Error,
+		PostModel,
+		PostModel | undefined
+	>((post) => api.likePost(post._id), {
+		onMutate: (updatedPost) => {
+			queryCache.cancelQueries(ALL_POSTS);
+
+			const posts = queryCache.getQueryData<PostModel[]>(ALL_POSTS) ?? [];
+			const found = posts.find((p) => p._id === updatedPost._id);
+			if (!found) {
+				console.error('Post not found!');
+				return undefined;
+			}
+			const snapshot = { ...found };
+
+			queryCache.setQueryData<PostModel[]>(ALL_POSTS, function (current) {
+				updatedPost.likeCount++;
+
+				if (current === undefined) return [updatedPost];
+
+				return current.map((p) => {
+					if (p._id === updatedPost._id) return updatedPost;
+					else return p;
+				});
+			});
+
+			return snapshot;
+		},
+		onError: (e, post, snapshot) => {
+			if (snapshot === undefined) return;
+
+			setError({ message: 'Could not like the post...', error: e });
+
+			const posts = queryCache.getQueryData<PostModel[]>(ALL_POSTS) ?? [];
+			const updatedPosts = posts.map((p) => {
+				if (p._id === snapshot._id) return snapshot;
+				return p;
+			});
+			queryCache.setQueryData<PostModel[]>(ALL_POSTS, updatedPosts);
+		},
+		onSuccess: (post) => {
+			const posts = queryCache.getQueryData<PostModel[]>(ALL_POSTS) ?? [];
+
+			posts.map((p) => {
+				if (p._id === post._id) return post;
+				return p;
+			});
+		},
+	});
+
+	return {
+		likePost: async (data: PostModel) => {
+			await likePost(data);
+		},
+		errorOnLike: error,
+	};
+}
